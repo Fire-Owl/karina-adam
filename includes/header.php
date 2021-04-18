71dfe0b74da67bdb66b9d195e99ad010130f9f8f
acs <k.el-yacoubi@codeur.online>
2021-04-18 14:46:11 +0200

    nav bar

--- includes/header.php
+++ includes/header.php
@@ -5,11 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web For Beginner</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="assets/images/logo.png" />
     <link href="assets/styles/style.css" rel="stylesheet">
-
-    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="assets/images/logo.png" />  
+    <link href="//db.onlinewebfonts.com/c/860c3ec7bbc5da3e97233ccecafe512e?family=Circular+Std+Book" rel="stylesheet" type="text/css"/>
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
