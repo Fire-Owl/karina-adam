dd0e5b888cc71bead72859346651c5ac91fd22e3
acs <a.bederiat@codeur.online>
2021-04-21 15:55:16 +0200

    changement

--- includes/header.php
+++ includes/header.php
@@ -5,9 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web For Beginner</title>
-    <link href="assets/styles/style.css" rel="stylesheet">
+    <link href="../assets/styles/style.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link rel="icon" type="image/png" href="assets/images/logo.jpg" />  
+    <link rel="icon" type="image/jpg" href="../assets/images/logo.jpg" />  
     <link href="//db.onlinewebfonts.com/c/860c3ec7bbc5da3e97233ccecafe512e?family=Circular+Std+Book" rel="stylesheet" type="text/css"/>
    
 </head>
@@ -15,7 +15,7 @@
     <nav class="navbar navbar-expand-lg bg-transparent">
         <div class="container-fluid">
             <a class="navbar-brand" href="#">
-                <img class="logo" src="assets/images/logo.png" alt="logo web for beginner"> 
+                <img class="logo" src="../assets/images/logo.jpg" alt="logo web for beginner"> 
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
