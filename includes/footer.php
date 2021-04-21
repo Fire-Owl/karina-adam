1e5c8868e1e12bbc1f31c0c3a4ad180cd624d8e2
acs <a.bederiat@codeur.online>
2021-04-21 14:12:52 +0200

    ordre

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<div class="bg-footer">
+   <a href="">Contactez-nous</a>
+   <a href="">Blog promo 68</a>
+   <a href="">Access Code School</a>
+   <a href="">Haut de page</a>
+   <p>Copyright @ 2021 Web For beginner - Tous droits réservés </p>
+   <a href="">Mentions légales</a>
+   <img src="assets/images/Facebook.png" alt="logo facebook">
+   <img src="assets/images/insta.png" alt="logo instagram">
+   <img src="assets/images/twitter.png" alt="logo twitter"> 
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web For Beginner</title>
+    <link href="assets/styles/style.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="assets/images/logo.jpg" />  
+    <link href="//db.onlinewebfonts.com/c/860c3ec7bbc5da3e97233ccecafe512e?family=Circular+Std+Book" rel="stylesheet" type="text/css"/>
+   
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg bg-transparent">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img class="logo" src="assets/images/logo.png" alt="logo web for beginner"> 
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#" id="metiers">Les métiers du web</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">L'univers du web</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav> 
