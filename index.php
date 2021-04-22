994ad98ccc35eabe9ef1897d37f20a7037088b7a
acs <k.el-yacoubi@codeur.online>
2021-04-22 13:51:28 +0200

    banniere

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php include 'includes/header.php' ; ?>
 
 
-
+<div class="banniere">
+    <img src="assets/images/banniere.png" alt="banniere">
+    <div class="text-lg-left">
+        <h1> Web for beginner</h1>
+        <p> Découvrez les métier du web</p>
+    </div>
+</div>
 
 
 <?php include 'includes/footer.php' ; ?>
