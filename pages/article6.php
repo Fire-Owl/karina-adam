f277ce003799a83c686b7211ef24bf421c3f6090
acs <a.bederiat@codeur.online>
2021-04-27 14:16:15 +0200

    artciles

---
+++ pages/article6.php
@@ -0,0 +1,64 @@
+<?php include "../includes/header.php"; ?>
+
+
+<div class="zoneblue">
+<!-- ici insertion img position absolute (+divbleu = position relative) et z-index -->
+
+
+<div class="container-fluid">
+<div class="row">
+<div class="col-md-3 col-sm-1 border-ad centerback"><img src="pictures/img-blue-left-opacity100.png" alt="" class="back-img blueleft"></div>
+<div class="col-md-6 col-sm-10 border-ad">
+<h1 class="h1-blue">Les agences de <br>communication</h1>
+<div><img src="../assets/images/agence_com.jpg" alt="photo article agence de com" class="photo"></div>
+</div>
+<div class="col-md-3 col-sm-1 border-ad centerback"><img src="pictures/img-blue-right-opacity100.png" alt="" class="back-img blueright"></div>
+</div>
+</div>
+</div>
+<div class="zoneblue2"></div>
+
+<div class="container"> 
+    <div class = "role">
+    <h1>Quelles sont les structures pourvoyeuses d’emploi dans le secteur du numérique ?<br><br></h1>
+    <p>Dans ce vaste panel d’entreprises et d’organisations, penchons-nous sur le cas des agences de communication : Le but de cette dernière consiste à apporter un soutien à une structure afin de lui procurer la position désirée en matière de politique relationnelle. L’agence de communication peut prendre en charge l’intégralité de la campagne de communication de ses clients ou se focaliser sur une partie des actions à mener. Certaines agences se spécialisent dans des types d’actions spécifiques alors que d’autres préfèrent proposer des services généralisées. <br><br>On distingue par ailleurs la communication externe et la communication interne : <br><br> La communication externe se traduit par l’ensemble des processus et actions menés à destination de la clientèle actuelle d’une entreprise et de sa clientèle potentielle (le grand public) –> l’agence de communication se charge de faire connaître l’entreprise qui l’a sollicité auprès des consommateurs et entretien la communication entre celle-ci et ses clients acquis. La communication externe fait partie intégrante de la construction de l’identité d’une entreprise et lui permet de se différencier de la concurrence. –> Il s’agit d’un indéniable pilier dans la construction de la notoriété d’une structure et de son image. <br><br> La communication interne peut être confiée à une agence de communication mais elle est généralement gérée en interne par les entreprises par leur service communication respectifs et/ou leur service RH. <br><br> Quels sont les différents métiers qui composent l’organigramme d’une agence de communication ? <br><br>Chef de publicité Le chef de publicité est un commercial de terrain. Il est en charge de la liaison entre l’agence de publicité et l’annonceur. Il effectue l’analyse du besoin du client et lui propose différentes stratégies de campagne. Dans un second temps, le chef de publicité coordonne les équipes de créatifs et les responsables média (media planneur). Il prend également en charge la supervision du suivi de la campagne de communication (diffusion, affichage …). La prospection en vue d’élargir la clientèle et contribuer au développement de l’agence.</p>
+    </div>
+    <div class ="pre">
+    <h1>Directeur artistique : <br><br></h1>
+    <p>Le travail du directeur artistique démarre dans la compréhension des attentes de ses clients. Que souhaite-t-il communiquer, quelle est la cible du produit ? Chaque cible réagissant différemment à un même message, l’aspect sociologique occupe une place importante dans cette profession : le directeur artistique doit être capable d’anticiper de quelle manière sera perçu sa communication par les segments de consommateurs ciblés. En outre, son métier requiert bien sûr de bonnes connaissances et compétences artistiques, un sens du “bon-goût” et une imagination conséquente pour innover sans relâche.</p>
+    </div>
+    <div class ="agence"><h1>Directeur de la clientèle : <br><br></h1>
+    <p>Le directeur de la clientèle est l’ambassadeur de l’agence auprès des clients dont celle-ci est en charge. Il se définit comme un commercial mais aussi comme un coordinateur, un conseiller, un gestionnaire et un manager. <br><br> Concepteur rédacteur <br><br> Il est en charge de la conception et de la rédaction des messages publicitaires. Son but est d’attirer l’attention du consommateur et de l’inciter à acheter. Le concepteur rédacteur travaille sur de multiples supports : print, publicité télévisée, radio, web … En collaboration avec le directeur artistique, qui est son homologue en création visuelle, il établit un plan de communication et cherche le slogan approprié. Plusieurs idées sont soumises au directeur de création et un seul sera présenté au client final.</p>
+    </div>
+    <div class = "agence">
+    <h1>Traffic manager : <br><br></h1>
+    <p>La fonction du Traffic Manager complète celle du Community Manager : le Traffic Manager a pour principal objectif de développer l’audience du site internet de l’entreprise et la base de nouveaux clients. Le métier de Traffic Manager est au croisement des métiers du Marketing et de la Communication.</p>
+    </div>
+    <div class = "agence">
+    <h1>Média planneur <br><br></h1>
+    <p>Le Média Planner est un spécialiste de la publicité dans les médias. Il planifie la diffusion de campagnes publicitaires vantant les mérites d’un produit spécifique sur les supports appropriés, susceptibles de toucher la cible de consommateurs visés : presse, télévision, radio, internet, affichage… Pour cela, il effectue une veille stratégique des médias ainsi que de la concurrence.</p>
+    </div>
+    <div class = "agence1">
+    <h1>Planneur stratégique <br><br></h1>
+    <p>La tâche du planneur stratégique est d’effectuer une veille économique mais aussi marketing des marchés dans le but de fournir de la matière à ses collaborateurs du pôle création et à ceux des pôles commerciaux. Son objectif réside en la construction du positionnement d’une marque en cohérence avec la stratégie de l’entreprise et les attentes des consommateurs potentiels.</p>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<?php include "../includes/footer.php"; ?>
+
+
